802eb37e4afebc295a82bd4ceaa1ccefb5cb74fd
Jose Rodriguez <jose.rodriguez@optelgroup.com>
2018-11-20 12:59:18 +0000

    Include Save and Restore App state functionality

--- app/src/main/java/io/github/jlrods/mytodolist/TasksDB.java
+++ app/src/main/java/io/github/jlrods/mytodolist/TasksDB.java
@@ -24,7 +24,11 @@ public class TasksDB extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         Log.d("Ent_DBOncreate","Enter onCreate method in TasksDB class.");
-
+        //Create table to store app state
+        db.execSQL("CREATE TABLE APP (_id INTEGER PRIMARY KEY AUTOINCREMENT,currentCategory INTEGER, isSearchFilter INTEGER, isChecked INTEGER," +
+                "lastSearchTask TEXT,lastSearchGrocery TEXT);");
+        //Populate default state of app
+        db.execSQL("INSERT INTO APP VALUES(null,1,0,0,'','')");
         //Create User table
         db.execSQL("CREATE TABLE USER (_id INTEGER PRIMARY KEY AUTOINCREMENT,UserName TEXT, UserMessage TEXT, Photo TEXT);");
         //Populate the user table with unique user
@@ -602,23 +606,6 @@ public class TasksDB extends SQLiteOpenHelper {
         return grocery;
     }//End of extractGrocery method
 
-   /* public Task extractGrocery(Cursor c,int id){
-        //Declare null Task object to be returned by method
-        Grocery grocery = null;
-        boolean found = false;
-        int i =0;
-        do{
-            if(c.getColumnIndexOrThrow("_id")== id){
-                grocery = extractGrocery(c);
-                found = true;
-            }else{
-                i++;
-            }
-        }while(!found && i<c.getCount());
-        return grocery;
-    }//End of extractTask method*/
-
-
     //Method to retrieve the list of categories stored on the database
     public ArrayList<Category> getCategoryList(){
         Log.d("Ent_getCategoryList","Enter getCategoryList method in the TaskDB class.");
@@ -892,7 +879,7 @@ public class TasksDB extends SQLiteOpenHelper {
         Log.d("Ent_UpdateUser","Enter the updateUser method in the TaskDB class.");
         boolean success = false;
         Cursor userData;
-                //Declare and instantiate a new database object to handle the database operations
+        //Declare and instantiate a new database object to handle the database operations
         SQLiteDatabase bd = getWritableDatabase();
         userData = this.runQuery("SELECT * FROM USER");
         String updateUser ="UPDATE USER SET ";
@@ -914,6 +901,38 @@ public class TasksDB extends SQLiteOpenHelper {
         Log.d("Ext_UpdateUser","Exit the updateUser method in the TaskDB class.");
         return success;
     }//End of updateUser method
+
+    public boolean updateAppState(int currentCategory,int isSearchFilter, int isChecked, String lastSearchTask, String lastSearchGrocery){
+        Log.d("Ent_UpdateState","Enter the updateAppState method in the TaskDB class.");
+        boolean success = false;
+        Cursor appState;
+        //Declare and instantiate a new database object to handle the database operations
+        SQLiteDatabase db = getWritableDatabase();
+        appState = this.runQuery("SELECT * FROM APP");
+        String updateState ="UPDATE APP SET ";
+        String fields = " currentCategory = " + currentCategory + ","+
+                        " isSearchFilter = " + isSearchFilter + ","+
+                        " isChecked = "+ isChecked + ","+
+                        " lastSearchTask = '" + lastSearchTask + ","+
+                        " lastSearchGrocery = '" + lastSearchGrocery;
+        String whereId = " WHERE _id = ";
+        String sql = "";
+        if(appState.moveToNext()){
+            sql = updateState+fields+ whereId+appState.getInt(0);
+        }
+        //Try Catch block to execute the sql command to update corresponding table
+        try{
+            //Run the query and change success to true if no issues
+            db.execSQL(sql);
+            success = true;
+        }catch (Exception e) {
+            //Log the exception message
+            Log.d("DataBaseException",e.toString());
+        }//End of try and catch block
+        Log.d("Ext_UpdateState","Exit the updateAppState method in the TaskDB class.");
+        return success;
+    }//End of updateAppState
+
 
 
 
